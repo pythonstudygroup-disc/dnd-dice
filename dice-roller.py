b3304acee83cc1fa493cb31674ec1974c748a5e7
pythonstudygroup-disc <68472200+pythonstudygroup-disc@users.noreply.github.com>
2020-07-18 10:50:40 -0400

    Update dice-roller.py

--- dice-roller.py
+++ dice-roller.py
@@ -2,7 +2,7 @@ import random
 import math
 
 print("Welcome, brave sir or madam or person! Choose your weapon..."
-      "\n\nInput the dice you need to roll as an integer."
+      "\n\nInput the die you need to roll as an integer."
       "\n\nie. If you need to roll a d20, type '20')")
 
 while True:
